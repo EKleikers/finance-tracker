d155beda98159394edc3dc773e8caf456ca1af61
esther-kleikers <esther.kleikers@aisler.team>
2026-08-09 17:06:50 +0200

    modifing device views & ctrl

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,9 @@
 class Stock < ApplicationRecord
   require "httparty"
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
 
+  validates :name, :ticker, presence: true
   def self.company_lookup(ticker_symbol)
     response = HTTParty.get(
       "https://finnhub.io/api/v1/stock/profile2",
@@ -32,5 +35,9 @@ class Stock < ApplicationRecord
       name: name,
       last_price: last_price
     )
+  end
+
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end
